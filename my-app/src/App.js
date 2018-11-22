cd1ca57bc8d1122786afd66cec874ff9ae3120d5
May Kong <1285235080@qq.com>
2018-11-23 11:11:07 +1300

    initialize

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import May from './May'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi May</h1>
+        <May />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/May.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>Name: May</div>
+        <div>Age: 26</div>
+      </div>
+    );
+  }
+}
+
+export default App;
