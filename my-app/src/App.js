6e088ac3feaeb02079a9f6197e7d416cdc614cd4
May Kong <1285235080@qq.com>
2018-11-23 15:52:44 +1300

    Update the state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import May from './May';
+import AppUser from './AppUser';
 
 class App extends Component {
   //state: List format
@@ -11,12 +12,25 @@ class App extends Component {
       {name: "Jenny", age: "14", nationality: "US", id: 4 }
     ]
   }
+  //add the user to the state
+  addUser = (infor) => {
+    console.log(infor)
+    infor.id = Math.random();
+    let originalInformation = [...this.state.information, infor]
+    this.setState({
+      information: originalInformation
+
+    })
+  }
   render() {
     //pass the state to the child component 
+    //The information and addUser is a middleware (props), they allow the the child/other component to access the state, function et
+    //create a Props, props
     return (
       <div className="App">
         <h1>Hi May</h1>
           <May information={this.state.information} />
+          <AppUser addUser={this.addUser}/>
       </div>
     );
   }
---
+++ my-app/src/AppUser.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class AppUser extends Component {
+  //state: List format
+  state = {
+    name: null,
+    age: null,
+    nationality: null
+  }
+  handleChange = (e) => {
+    this.setState({
+      //the name of the id for each field is the same as the state field on line 6,7,8
+        [e.target.id]: e.target.value
+    })
+  }
+  handleSummit = (e) => {
+    e.preventDefault();
+    //when summit button was clicked, the information is sent to the console
+    //but how to update the state in App.js?
+    //
+    //console.log(this.state)
+    //there are two props in App.js now, the information and addUser, we are going to use the addUser one, (this.state) means using the state in AppUser.js
+    this.props.addUser(this.state);
+  }
+
+  render() {
+    //pass the state to the child component 
+    //onChange function is gonna update the state
+    return (
+      <div className="AppUser" onSubmit={this.handleSummit}>
+        <form action="">
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" onChange={this.handleChange}/>
+            
+            <label htmlFor="age">Age: </label>
+            <input type="text" id="age" onChange={this.handleChange}/>
+            
+            <label htmlFor="nationality">Nationality: </label>
+            <input type="text" id="nationality" onChange={this.handleChange}/>
+
+            <button>Summit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AppUser;
